83c3ed14e3beb56b7889447066f96867bf26699d
Luciano E. Perez <info.luciando.design@gmail.com>
2024-04-05 00:14:29 -0300

    changes cyclic4

--- api/index.js
+++ api/index.js
@@ -17,17 +17,17 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001;
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const port = process.env.PORT || 3000;
 
 // Syncing all the models at once.
-conn.sync({ force: true})
-.then(() => {
-  
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-  });
-})
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+    });
+  })
 
-.catch((err) => console.log(err));
+  .catch((err) => console.log(err));
